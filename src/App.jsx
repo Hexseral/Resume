f7e01b3972a9e5594e3abf226548cd8647d028d8
Jordan Tatum <jrtatum0133@eagle.fgcu.edu>
2022-03-28 21:13:20 -0400

    unique objects with animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
-import React from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import React, {useRef} from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import './App.css'
+import { MeshWobbleMaterial, OrbitControls } from "@react-three/drei";
 
-function Box(){
+const SpinningMesh = ({ position, args, color }) => {
+  const mesh = useRef(null);
+  useFrame(() =>(mesh.current.rotation.x = mesh.current.rotation.y = mesh.current.rotation.z += 0.01));
   return (
-    <mesh> 
-      <boxBufferGeometry attach="geometry"  />
-      <meshNormalMaterial attach="material"  />
+    <mesh position={position} ref ={mesh}> 
+      <boxBufferGeometry attach="geometry" args={[args]} />
+      <MeshWobbleMaterial attach="material" color={color} speed={5} factor={1.5} wireframe={true}  />
     </mesh>
-  )
-}
+  );
+};
 
-export default function App() {
+function App() {
   return (
    <>
-      <Canvas>
+      <Canvas colorManagment camera={{position: [-5, 2, 10], fov: 60}}>
+        <ambientLight intensity={0.3} />
+        <SpinningMesh position={[0, 1, 0]} color="pink" />
+        <SpinningMesh position={[-2, 1, -5]} color="green"/>
+        <SpinningMesh position={[5, 1, -2]} color="blue"/>
         <OrbitControls />
-        <ambientLight intensity={0.5} />
-        <spotLight position={[10, 15, 10]} angle={0.3} />
-        <Box />
       </Canvas>
    </>
   );
 }
 
+export default App;
 
